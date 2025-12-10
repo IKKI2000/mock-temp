884b73f250c903bf9caaf62b9d82c45b992a5612
IKKI2000 <ikki2002@qq.com>
2025-12-10 10:49:54 +0800

    feat(仓库): 新增模拟数据服务~

---
+++ index.js
@@ -0,0 +1,31 @@
+import { createServer } from 'node:http';
+import connect from 'connect';
+import corsMiddleware from 'cors';
+import { baseMiddleware, createLogger, mockWebSocket } from 'vite-plugin-mock-dev-server/server';
+import mockData from './mock-data.js';
+
+const app = connect();
+const server = createServer(app);
+const logger = createLogger('mock-server', 'error');
+const proxies = ["/admin-api/"];
+const wsProxies = [];
+const cookiesOptions = {};
+const bodyParserOptions = {};
+const priority = {};
+const compiler = { mockData }
+
+mockWebSocket(compiler, server, { wsProxies, cookiesOptions, logger });
+
+app.use(corsMiddleware());
+app.use(baseMiddleware(compiler, {
+  formidableOptions: { multiples: true },
+  proxies,
+  priority,
+  cookiesOptions,
+  bodyParserOptions,
+  logger,
+}));
+
+server.listen(8080);
+
+console.log('listen: http://localhost:8080');
